9033682043381c86ade2d2079036d2363169888d
DianaDorneles <dianadorneles@hotmail.it>
2013-11-12 23:26:38 +0100

    Modificato il progetto appena creato

--- New folder/NewGitHubProject/src/NewGitHubProject.cpp
+++ New folder/NewGitHubProject/src/NewGitHubProject.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "This is a project that will be uploaded on GitHub!" << endl; // prints !!!Hello World!!!
+	cout << "Modified!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
