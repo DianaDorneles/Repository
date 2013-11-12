88d332ca14ddff86b01c426ca021faecda66f341
DianaDorneles <dianadorneles@hotmail.it>
2013-11-12 23:05:49 +0100

    Inserito un nuovo progetto su GitHub

---
+++ New folder/NewGitHubProject/src/NewGitHubProject.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : NewGitHubProject.cpp
+// Author      : Diana
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This is a project that will be uploaded on GitHub!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
